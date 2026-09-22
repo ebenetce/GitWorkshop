7fcd07363f8c0fb8f3783c688695292210340280
Edu Benet Cerda <ebenetce@mathworks.com>
2026-09-22 15:33:55 +0200

    removed redundant

--- tbx/svar/Contents.m
+++
@@ -1,7 +0,0 @@
-% SVAR - Structural Vector Autoregression Toolbox
-% Version 1.0.0 21-Sep-2026
-%
-% Folders
-%   svar     - Estimate and analyse structural vector autoregression models.
-%
-% See also VARM
